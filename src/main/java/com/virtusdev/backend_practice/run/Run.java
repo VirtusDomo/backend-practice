e0cff8528657ef2385bfe0a85d136312a753c512
VirtusDomo <abanybe@gmail.com>
2024-05-26 14:33:27 -0500

    Debugging Run Record Issues

--- src/main/java/com/virtusdev/backend_practice/run/Run.java
+++ src/main/java/com/virtusdev/backend_practice/run/Run.java
@@ -1,15 +1,23 @@
 package com.virtusdev.backend_practice.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 public record Run (
     Integer id,
+    @NotEmpty
     String title,
     LocalDateTime startedOn,
     LocalDateTime completedOn,
+    @Positive
     Integer miles,
     Location location
 ){
-
+    public Run(){
+        if(!completedOn.isAfter(startedOn)){
+            throw new IllegalArgumentException("Complete On must be after Started On");
+        }
+    }
     
 }
